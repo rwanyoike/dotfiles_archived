16d1bab70e7049a056b4175cd0c744a356c79204
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2017-02-22 14:13:09 +0300

    Shell emacs style

--- zsh/keys.plugin.zsh
+++ zsh/keys.plugin.zsh
@@ -2,10 +2,13 @@
 # http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Zle-Builtins
 # http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Standard-Widgets
 
+# Use emacs key bindings
+bindkey -e
+
 # [Ctrl-r] - Search backward incrementally for a specified string. The string
 # may begin with ^ to anchor the search to the beginning of the line.
-bindkey '^r' history-incremental-search-backward
+bindkey '^R' history-incremental-search-backward
 
 # emacs style
-bindkey '^a' beginning-of-line
-bindkey '^e' end-of-line
+bindkey '^A' beginning-of-line
+bindkey '^E' end-of-line

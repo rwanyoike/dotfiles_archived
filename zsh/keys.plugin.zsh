cee8f56669051178439591c98add027440dcd3c0
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2017-04-04 10:48:33 +0300

    Clean the house

--- zsh/keys.plugin.zsh
+++ zsh/keys.plugin.zsh
@@ -1,14 +1,12 @@
 # http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html
-# http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Zle-Builtins
-# http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Standard-Widgets
 
-# Use emacs key bindings
+# Use Emacs key bindings.
 bindkey -e
 
 # [Ctrl-r] - Search backward incrementally for a specified string. The string
 # may begin with ^ to anchor the search to the beginning of the line.
 bindkey '^R' history-incremental-search-backward
 
-# emacs style
+# Emacs style.
 bindkey '^A' beginning-of-line
 bindkey '^E' end-of-line

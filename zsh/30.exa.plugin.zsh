cc814fe82981e20a520b93a9c1121d06a099ce15
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2017-11-06 16:06:27 +0300

    Update aliases, drop android-sdk

--- zsh/30.exa.plugin.zsh
+++ zsh/30.exa.plugin.zsh
@@ -3,12 +3,12 @@
 if hash exa 2> /dev/null; then
     alias ls="exa"
     # Aliases adapted from https://github.com/Eriner/zim.
-    alias l="exa --long --all --group"  # all files, human-readable sizes
+    alias l="ls --long --group --git --all"      # all files, human-readable sizes
     [[ -n ${PAGER} ]] && alias lm="l | ${PAGER}" # all files, human-readable sizes, use pager
-    alias ll="exa --long --group"       # human-readable sizes
-    alias lr="ll --recurse"             # human-readable sizes, recursive
-    alias lx="ll --sort=extension"      # human-readable sizes, sort by extension
-    alias lk="ll --sort=size"           # human-readable sizes, largest last
-    alias lt="ll --sort=modified"       # human-readable sizes, most recent last
-    alias lc="lt --sort=created"        # human-readable sizes, most recent last, change time
+    alias ll="ls --long --group --git"           # human-readable sizes
+    alias lr="ll --recurse"                      # human-readable sizes, recursive
+    alias lx="ll --sort=extension"               # human-readable sizes, sort by extension
+    alias lk="ll --sort=size"                    # human-readable sizes, largest last
+    alias lt="ll --sort=modified"                # human-readable sizes, most recent last
+    alias lc="lt --sort=created"                 # human-readable sizes, most recent last, change time
 fi

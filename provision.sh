61d47a2099aa8a2bdb4b0c9e92b383bc5a2fa69a
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2019-04-24 20:16:43 +0300

    Fix provision.sh to work in tmux; `tput dim` fails

--- provision.sh
+++ provision.sh
@@ -3,29 +3,29 @@
 set -o errexit
 set -o nounset
 
-repo_path="$( cd "${BASH_SOURCE[0]%/*}" && pwd )"
+repo_path="$(cd "${BASH_SOURCE[0]%/*}" && pwd)"
 
-ui_bold=$(tput bold)
-ui_dim=$(tput dim)
-ui_green=$(tput setaf 2)
 ui_reset=$(tput sgr0)
+ui_002=$(tput setaf 2)
+ui_003=$(tput setaf 3)
+ui_bold=$(tput bold)
 
 # -----------------------------------------------------------------------------
 
 function dotfiles_log() {
   message="${1}"
 
-  printf "${ui_bold}${ui_green}%s${ui_reset}\n" "${message}"
+  printf "${ui_bold}${ui_002}%s${ui_reset}\n" "${message}"
 }
 
 function dotfiles_exec() {
   cmd="${1}"
 
-  printf "${ui_dim}$ %s${ui_reset}\n" "${cmd}"
+  printf "${ui_003}$ %s${ui_reset}\n" "${cmd}"
   eval "${cmd}"
 }
 
-function dotfiles_github() {
+function dotfiles_gh() {
   repo="${1}"
   dest="${2}"
   version="${3:-master}"
@@ -57,7 +57,7 @@ function dotfiles_mkdir() {
   fi
 }
 
-function dotfiles_setup() {
+function dotfiles_link() {
   files="${1}"
 
   find "${files}" -type f | while read -r "fname"; do
@@ -71,44 +71,44 @@ function dotfiles_setup() {
 
 # spacemacs
 dotfiles_log "--> spacemacs"
-dotfiles_github "syl20bnr/spacemacs" "${HOME}/.emacs.d" "develop"
+dotfiles_gh "syl20bnr/spacemacs" "${HOME}/.emacs.d" "develop"
 
 # vimrc
 dotfiles_log "--> vimrc"
-dotfiles_github "amix/vimrc" "${HOME}/.vim_runtime"
+dotfiles_gh "amix/vimrc" "${HOME}/.vim_runtime"
 
 # tmux
 dotfiles_log "--> tmux"
-dotfiles_github "gpakosz/.tmux" "${HOME}/.tmux"
+dotfiles_gh "gpakosz/.tmux" "${HOME}/.tmux"
 dotfiles_ln "${HOME}/.tmux/.tmux.conf" "${HOME}/.tmux.conf"
 
 # zplug
 dotfiles_log "--> zplug"
-dotfiles_github "zplug/zplug" "${HOME}/.zplug"
+dotfiles_gh "zplug/zplug" "${HOME}/.zplug"
 
 # pyenv
 dotfiles_log "--> pyenv"
-dotfiles_github "yyuu/pyenv" "${HOME}/.pyenv"
-dotfiles_github "yyuu/pyenv-virtualenv" "${HOME}/.pyenv/plugins/pyenv-virtualenv"
+dotfiles_gh "yyuu/pyenv" "${HOME}/.pyenv"
+dotfiles_gh "yyuu/pyenv-virtualenv" "${HOME}/.pyenv/plugins/pyenv-virtualenv"
 
 # rbenv
 dotfiles_log "--> rbenv"
-dotfiles_github "rbenv/rbenv" "${HOME}/.rbenv"
-dotfiles_github "rbenv/ruby-build" "${HOME}/.rbenv/plugins/ruby-build"
+dotfiles_gh "rbenv/rbenv" "${HOME}/.rbenv"
+dotfiles_gh "rbenv/ruby-build" "${HOME}/.rbenv/plugins/ruby-build"
 
 # nodenv
 dotfiles_log "--> nodenv"
-dotfiles_github "nodenv/nodenv" "${HOME}/.nodenv"
-dotfiles_github "nodenv/node-build" "${HOME}/.nodenv/plugins/node-build"
+dotfiles_gh "nodenv/nodenv" "${HOME}/.nodenv"
+dotfiles_gh "nodenv/node-build" "${HOME}/.nodenv/plugins/node-build"
 
 # goenv
 dotfiles_log "--> goenv"
-dotfiles_github "syndbg/goenv" "${HOME}/.goenv"
+dotfiles_gh "syndbg/goenv" "${HOME}/.goenv"
 
 # system_All
 dotfiles_log "--> system_All"
-dotfiles_setup "${repo_path}/dotfiles/system_All"
+dotfiles_link "${repo_path}/dotfiles/system_All"
 
 # system_$(uname)
 dotfiles_log "--> system_$(uname)"
-dotfiles_setup "${repo_path}/dotfiles/system_$(uname)"
+dotfiles_link "${repo_path}/dotfiles/system_$(uname)"

cb60d5f5a4afd52b132736ab22539677b659abd2
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2019-04-24 20:19:28 +0300

    Quote them all

--- dotfiles/system_Linux/config/redshift/hooks/notify.sh
+++ dotfiles/system_Linux/config/redshift/hooks/notify.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-redshift_event=$1
+redshift_event="${1}"
 
 case $redshift_event in
   period-changed)
-    # prev_period=$2
-    curr_period=$3
+    # prev_period="${2}"
+    curr_period="${3}"
     exec notify-send "Redshift" "Period changed to '${curr_period}'"
 esac

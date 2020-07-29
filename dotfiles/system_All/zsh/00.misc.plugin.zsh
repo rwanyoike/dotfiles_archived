9e3795ccf6fd656c5fa75a5569ea3529bfd2c35a
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2020-07-29 11:35:05 +0300

    s/ kitty with alacritty; redshift with gammastep; password-store with gopass
    Add intel-undervolt, kubectx, wf-recorder
    
    gammastep ticket: https://github.com/jonls/redshift/pull/663

--- dotfiles/system_All/zsh/00.misc.plugin.zsh
+++ dotfiles/system_All/zsh/00.misc.plugin.zsh
@@ -1,12 +1,14 @@
 # See: http://zsh.sourceforge.net/Doc/Release/Parameters.html
 #      http://zsh.sourceforge.net/Doc/Release/Options.html
-
-export HISTFILE="${HOME}/Sync/zhistory"
 export HISTSIZE=50000
 export SAVEHIST="${HISTSIZE}"
+export HISTFILE="${HOME}/Sync/zhistory"
 
-export VISUAL=vim
-
+# See: https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CONFIG_HOME="${HOME}/.config"
 export XDG_CACHE_HOME="${HOME}/.cache"
+
+# See: https://wiki.archlinux.org/index.php/Environment_variables
+export VISUAL=vim
+export EDITOR="${VISUAL}"
